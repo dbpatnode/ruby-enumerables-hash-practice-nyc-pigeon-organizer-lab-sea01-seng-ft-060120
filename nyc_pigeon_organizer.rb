25bce8d60881dcc8bfa3095afeb6d3820f0c7505
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-17 23:50:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,16 @@ organized_pigeons = {}
   data.each do |original_key, orignal_value|
     data.each do |new_key, new_value|
       new_value.each do |name|
-        if child_value.include?(name)
-        if not organized_pigeons[name]
+        if 
+          child_value.include?(name)
+        if not
+          organized_pigeons[name]
           organized_pigeons[name] = {original_key: [new_key.to_s]}
-        else if not organized_pigeons[name].key?(original_key)
+        else if not 
+          organized_pigeons[name].key?(original_key)
           organized_pigeons[name][original_key] = [new_key.to_s]
         else 
+          
         end
       end
     end

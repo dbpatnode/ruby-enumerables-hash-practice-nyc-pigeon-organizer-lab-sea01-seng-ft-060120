65be40c5e5f5a75cd0c3bc7bf90daf58ff77dfc6
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-17 23:38:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,5 @@
 def nyc_pigeon_organizer(data)
 organized_pigeons = {}
-  data.each do |name, color, gender, lives|
-    organized_pigeons[name] = {color: [], gender: [] lives:[]}
-  end
-  end
+  
 organized_pigeons
 end

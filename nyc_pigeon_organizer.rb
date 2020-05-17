d5e30ecbf66cf0b2e4ce7f0d984ac70af1f2b88e
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-17 23:40:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,11 @@
 def nyc_pigeon_organizer(data)
 organized_pigeons = {}
   data.each do |original_key, orignal_value|
-    data.each do |new_key, new_value
+    data.each do |new_key, new_value|
+      new_value.each do |name|
+        
+      end
+    end
   end
 organized_pigeons
 end

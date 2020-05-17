015b5a8180e8f2b3ee2411922744c8fd7b58bf94
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-17 23:45:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,7 @@ organized_pigeons = {}
       new_value.each do |name|
         if child_value.include?(name)
         if not organized_pigeons[name]
+        organized_pigeons[]
         else if not organized_pigeons
         else 
         end

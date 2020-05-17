7345d4a7ad7b8dd724a90a86e7b81aca52fba57a
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-17 23:49:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ organized_pigeons = {}
         if not organized_pigeons[name]
           organized_pigeons[name] = {original_key: [new_key.to_s]}
         else if not organized_pigeons[name].key?(original_key)
-          
+          organized_pigeons
         else 
         end
       end
